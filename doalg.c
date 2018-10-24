89c0a4750257dfcbfca30819b9f27670c7c7fc55
Pranav <pranavudupa94@gmail.com>
2018-10-24 02:10:53 -0700

    Fix variable names

--- doalg.c
+++ doalg.c
@@ -1,6 +1,3 @@
-#include <string.h>
-#define MAXK 100
-
 void insert_val_and_shift_right(int index, int val, int Best[], int K) {
     /*
      * Function: insert_val_and_shift_right
@@ -30,24 +27,22 @@ int bin_ins(int low, int high, int val, int Best[]) {
 }
 
 int doalg(int n, int K, int Best[]) {
-    memset(Best, 0, 100);
-    int i;
+    int item;
 
     // Insert first k items into Best in a reverse sorted fashion.
-    for (i = 1; i <= K; i++) {
-        int insert_index = bin_ins(0, i - 1, i, Best);
-        insert_val_and_shift_right(insert_index, i, Best, K);
+    for (item = 1; item <= K; item++) {
+        int insert_index = bin_ins(0, item - 1, item, Best);
+        insert_val_and_shift_right(insert_index, item, Best, K);
     }
 
-    // Iterate through all the items and insert to Best if required.
-    for (i = K + 1; i <= n; i++) {
-        // If the item is smaller than our smallest Best element, ignore and continue.
-        if (COMPARE(Best[K - 1], i) == 1)
-            continue;
-        int insert_index = bin_ins(0, K - 1, i, Best);
-        insert_val_and_shift_right(insert_index, i, Best, K);
+    // Iterate through all the items and insert to Best if 
+	// the item is bigger than the smallest Best element.
+    for (item = K + 1; item <= n; item++) {
+        if (COMPARE(Best[K - 1], item) == 2) {
+			int insert_index = bin_ins(0, K - 1, item, Best);
+			insert_val_and_shift_right(insert_index, item, Best, K);
+		}
     }
 
     return 1;
-
 }
